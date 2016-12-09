c72d1851c16121759f963d753074f0bd48982e29
Simeon Warner <simeon.warner@cornell.edu>
2016-12-09 17:06:51 -0500

    Trivial test for travis

---
+++ tests/test_validator.py
@@ -0,0 +1,19 @@
+"""Test code for iiif-presentation-validator.py."""
+import unittest
+import imp
+from bottle import Response
+
+# The validator isn't a module but with a little magic
+# we can load it up as if it were in order to access
+# the classes within
+fh = open('iiif-presentation-validator.py', 'r')
+try:
+    val_mod = imp.load_module('ipv', fh, '', ('py','r',imp.PY_SOURCE))
+finally:
+    fh.close()
+
+class TestAll(unittest.TestCase):
+
+    def test01_get_bottle_app(self):
+        v = val_mod.Validator()
+        self.assertTrue(v.get_bottle_app())
