1c3c64ce669f994580db9a7fad7c525b578e4e9d
Glen Robson <glen.robson@gmail.com>
2020-05-27 01:09:51 +0100

    Pinning pyld version for python 2.7

--- setup.py
+++ setup.py
@@ -56,7 +56,8 @@ setup(
         ':python_version<"3.0"': [
             "Pillow==6.2.2",
             "zipp==1.2.0",
-            "configparser==4.0.2"
+            "configparser==4.0.2",
+            "pyld=1.0.5"
         ],
     },
     test_suite="tests",
